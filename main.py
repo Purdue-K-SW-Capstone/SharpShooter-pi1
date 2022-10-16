e5a9eb874b29f836e68a4fd58174ebc4115c8ce8
8471919 <7471919@naver.com>
2022-10-16 14:38:51 -0400

    fix: seperate test code and product code start

--- main.py
+++ main.py
@@ -2,71 +2,83 @@ from picam.load_cam import Cam
 from lora.node_100_main import LoRa
 import time
 
+# Seding coordinate code
 def main():
     
-    # print("open the cam")
-    # cam = Cam()
+    print("open the cam")
+    cam = Cam()
     
     # This LoRa can transmit 240bytes per packet. (maybe)
+    print("open the LoRa")
+    lora = LoRa()
+        
+    while True:
+        # #This code is for coordinates
+        
+        #when sound sensor detect the gun sound
+        print("ready to capture")
+        
+        cam.capture()
+        print("------------capture-----------")
+        coordinates = cam.processing()
+        print("----------get coordinates---------")
+        
+        lora.transmit(coordinates)
+        
+        print("Complete")
+                
+        time.sleep(2)
+        
+        
+# Sending Image code
+def mainImage():       
+     
+    print("open the cam")
+    cam = Cam()
+    
+    # This LoRa can transmit 240bytes per packet. (maybe)
+    print("open the LoRa")
+    lora = LoRa()
+        
+    while True:        
+        #This code is for Transmitting Image
+        
+        print("ready to capture")
+        
+        pre = time.time()
+        print("------------capture-----------")
+        cam.capture()
+        print("----------get image---------")
+        image = cam.getImage()
+        
+        lora.transmitImage(image)
+        
+        aft = time.time()
+        
+        print(str(aft - pre) + " : seconds")
+        print("Complete")
+        
+        time.sleep(10)
+
+    
+    
+#This code is for Test
+def mainTest():
+    
     print("open the LoRa")
     lora = LoRa()
     
     INDEX = 1
     
-    while True:
-        # #This code is for coordinates
-        
-        # #when sound sensor detect the gun sound
-        # print("ready to capture")
-        
-        # # pre = time.time()
-        # cam.capture()
-        # print("------------capture-----------")
-        # coordinates = cam.processing()
-        # print("----------get coordinates---------")
-        
-        # lora.transmit(coordinates)
-        # # aft = time.time()
-        
-        # # print(str(aft - pre) + " : seconds")
-        # print("Complete")
-                
-        # time.sleep(2)
-        
-        
-        
-        
-        # #This code is for Transmitting Image
-        
-        # print("ready to capture")
-        
-        # pre = time.time()
-        # print("------------capture-----------")
-        # cam.capture()
-        # print("----------get image---------")
-        # image = cam.getImage()
-        
-        # lora.transmitImage(image)
-        
-        # aft = time.time()
-        
-        # print(str(aft - pre) + " : seconds")
-        # print("Complete")
-        
-        # time.sleep(10)
-        
-        
-        
-        #This code is for Test
+    while True: 
         lora.sendTest(INDEX)
         INDEX+=1
         print("---------complete---------")
         time.sleep(3)
-        
+            
     
     
 if __name__ == "__main__":
     main()
-
-
-
+    # mainImage()
+    # mainTest()
